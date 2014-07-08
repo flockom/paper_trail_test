24486fd3a69b13176732664cbc9a19ca48a76983
Frank Lockom <frank.lockom@northwestern.edu>
2014-07-08 10:13:46 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PaperTrailTest::Application.config.secret_token = '1d71c10bdafa5c4ed332bafa4ef14288564b899f096ee15ba7ea18bcaa59bbded787cc5c9c9f581ff67f12763d17c0500801433fe7e36b45e9e2027bcaa8016e'
